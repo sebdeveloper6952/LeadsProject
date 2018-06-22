f3bc3ab452b6a8a77d9c1bd4dd300bfeb69806a6
Sebastian Arriola <sebdeveloper6952@gmail.com>
2018-06-21 18:49:11 -0600

    Filter by category.
    Filter dummy items using a filter action on the action bar.

--- app/src/main/java/com/projects/sebdeveloper6952/chapinleads/dummy/ItemLead.kt
+++ app/src/main/java/com/projects/sebdeveloper6952/chapinleads/dummy/ItemLead.kt
@@ -8,24 +8,27 @@ object DummyData {
 
     var LEADS: ArrayList<ItemLead>
     var RECS: ArrayList<ItemLead>
+    var CATEGORIES: Array<String>
 
     init {
         LEADS = arrayListOf(
-                ItemLead("Lead1", "Lead1 details...", "category" ,R.drawable.ic_image_black_24dp),
-                ItemLead("Lead2", "Lead2 details...", "category" ,R.drawable.ic_image_black_24dp),
-                ItemLead("Lead3", "Lead3 details...", "category" ,R.drawable.ic_image_black_24dp),
-                ItemLead("Lead4", "Lead4 details...", "category" ,R.drawable.ic_image_black_24dp),
-                ItemLead("Lead5", "Lead5 details...", "category" ,R.drawable.ic_image_black_24dp),
-                ItemLead("Lead6", "Lead6 details...", "category" ,R.drawable.ic_image_black_24dp)
+                ItemLead("Lead1", "Lead1 details...", "Tecnología" ,R.drawable.ic_image_black_24dp),
+                ItemLead("Lead2", "Lead2 details...", "Tecnología" ,R.drawable.ic_image_black_24dp),
+                ItemLead("Lead3", "Lead3 details...", "Deportes" ,R.drawable.ic_image_black_24dp),
+                ItemLead("Lead4", "Lead4 details...", "Deportes" ,R.drawable.ic_image_black_24dp),
+                ItemLead("Lead5", "Lead5 details...", "Tecnología" ,R.drawable.ic_image_black_24dp),
+                ItemLead("Lead6", "Lead6 details...", "Finanzas" ,R.drawable.ic_image_black_24dp)
         )
         RECS = arrayListOf(
-                ItemLead("nVidia", "American technology company. Designs GPUs", "Technology" ,R.drawable.ic_image_black_24dp),
-                ItemLead("Recommendation2", "Recommendation2 details...", "category" ,R.drawable.ic_image_black_24dp),
-                ItemLead("Recommendation3", "Recommendation3 details...", "category" ,R.drawable.ic_image_black_24dp),
-                ItemLead("Recommendation4", "Recommendation4 details...", "category" ,R.drawable.ic_image_black_24dp),
-                ItemLead("Recommendation5", "Recommendation5 details...", "category" ,R.drawable.ic_image_black_24dp),
-                ItemLead("Recommendation6", "Recommendation6 details...", "category" ,R.drawable.ic_image_black_24dp)
+                ItemLead("Nvidia", "American technology company. Designs GPUs", "Tecnología" ,R.drawable.ic_image_black_24dp),
+                ItemLead("Recommendation2", "Recommendation2 details...", "Tecnología" ,R.drawable.ic_image_black_24dp),
+                ItemLead("Recommendation3", "Recommendation3 details...", "Deportes" ,R.drawable.ic_image_black_24dp),
+                ItemLead("Recommendation4", "Recommendation4 details...", "Deportes" ,R.drawable.ic_image_black_24dp),
+                ItemLead("Recommendation5", "Recommendation5 details...", "Deportes" ,R.drawable.ic_image_black_24dp),
+                ItemLead("Recommendation6", "Recommendation6 details...", "Finanzas" ,R.drawable.ic_image_black_24dp)
         )
+
+        CATEGORIES = arrayOf("Tecnología", "Deportes", "Psicología", "Finanzas")
     }
 
     fun addLead(newLead: ItemLead) = LEADS.add(newLead)
